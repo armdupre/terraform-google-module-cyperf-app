fab7bb50c7d3f7f4e5b30cffac3582fee6f18500
armdupre <a-joseph.dupre@keysight.com>
2023-06-09 12:49:19 -0400

    Initial Checkin.

---
+++ locals.tf
@@ -0,0 +1,21 @@
+locals {
+	CustomImageProjectId = var.CustomImageProjectId
+	CustomImageFamilyName = var.CustomImageFamilyName
+	Eth0PublicIpAddressName = "${local.Preamble}-eth0-ip-addr"
+	Eth0SubnetName = var.Eth0SubnetName
+	Eth0VpcNetworkName = var.Eth0VpcNetworkName
+	InstanceId = var.InstanceId
+	InstanceName = "${local.Preamble}-instance"
+	MachineType = var.MachineType
+	NetworkTargetTags = var.NetworkTargetTags
+	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.Tag}-${local.Version}-${local.InstanceId}"
+	RegionName = var.RegionName
+	SerialPortEnable = true
+	SleepDelay = var.SleepDelay
+	Tag = var.Tag
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+	ZoneName = var.ZoneName
+	Version = var.Version
+}
